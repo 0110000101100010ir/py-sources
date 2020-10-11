151029ef3cdf167c61ea0572d404eb86bcfbe1d3
abir <0110000101100010ir@gmail.com>
2020-10-12 02:03:12 +0530

    new commit

--- first.py
+++ first.py
@@ -1,12 +1,12 @@
 # heron of alexandria greek
 # find sqaure root of a number
-# by abir
-import math
+import time
 square_root_of = float(input("SQUARE ROOT OF: "))
 precision = float(input("PRECISION: "))
 if square_root_of < 0:
     print("SQUARE ROOT OF A NUMBER LESS THAN ZERO IS NOT REAL.")
 else:
+    tic = time.time()
     upper = square_root_of + precision
     lower = square_root_of - precision
     guess = 2
@@ -18,6 +18,14 @@ else:
             break
         else:
             guess = (guess + (square_root_of/guess))/2
+    tac = time.time()
+    print("{:.3f}".format(square_root))
+    print(f"COMPUTATION TIME: {tac-tic} ")
 
-    print(square_root)
-    print(math.sqrt(square_root_of))
+    #testing result
+    import math
+    tic = time.time()
+    check = math.sqrt(square_root_of)
+    tac = time.time()
+    print("{:.3f}".format(check))
+    print(f"COMPUTATION TIME: {tac-tic}")
